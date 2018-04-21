ec11736ab7bc73064808f48a4a39c2dcb1c8f5ce
Rafael Zambelli Meneghelo <rmeneghelo@gmail.com>
2018-04-20 23:53:25 -0300

    travis

--- R6Api/Program.cs
+++ R6Api/Program.cs
@@ -13,11 +13,11 @@ namespace R6Api
         public static void Main(string[] args)
         {
             var host = new WebHostBuilder()
-                .UseKestrel()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
-                .UseStartup<Startup>()
-                .Build();
+              .UseKestrel()
+              .UseContentRoot(Directory.GetCurrentDirectory())
+              .UseIISIntegration()
+              .UseStartup<Startup>()
+              .Build();
 
             host.Run();
         }
